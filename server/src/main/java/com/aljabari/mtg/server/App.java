af3689ee8053e594379679a4b2acdc71b6d9762e
Jack <jabavaaljabari@gmail.com>
2025-09-15 00:01:06 +0100

    Basic maven setup

---
+++ server/src/main/java/com/aljabari/mtg/server/App.java
@@ -0,0 +1,10 @@
+package com.aljabari.mtg.server;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
